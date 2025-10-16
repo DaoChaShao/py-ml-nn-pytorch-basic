c2a605e4b1f7e32346db2a363c963f4593b5f75e
DaoChaShao <27882272@qq.com>
2025-10-16 22:01:22 +0800

    feat: add 17_auto_grad_01.py for demonstrating tensor operations and backpropagation in PyTorch

---
+++ 17_auto_grad_01.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/16 15:38
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   17_auto_grad_01.py
+# @Desc     :   
+
+from torch import Tensor, tensor, rand, float64, nn
+
+from utils.highlighter import green, red
+from utils.helper import RandomSeed, Beautifier
+
+
+def main() -> None:
+    """ Main Function """
+    with RandomSeed("Simple Network"):
+        # Define a tensor as a feature
+        X: Tensor = tensor(10, dtype=float64, requires_grad=True)
+        y: Tensor = tensor([[3]], dtype=float64, requires_grad=True)
+        with Beautifier("Feature and Target Tensors"):
+            print(f"Feature Tensor: {X}")
+            print(f"Target Tensor: {y}")
+
+        # Define the weights and bias
+        W: Tensor = rand(1, 1, dtype=float64, requires_grad=True)
+        b: Tensor = rand(1, 1, dtype=float64, requires_grad=True)
+        with Beautifier("Weights and Bias Tensors"):
+            print(f"Weights Tensor: {W}")
+            print(f"Bias Tensor: {b}")
+
+        # Forward pass: compute the predicted y
+        Z: Tensor = X * W + b
+        with Beautifier("Predicted Tensor"):
+            print(f"Predicted Tensor: {Z}")
+
+        # Compute and print the loss
+        loss_fn: nn.MSELoss = nn.MSELoss()
+        loss: Tensor = loss_fn(Z, y)
+        with Beautifier("Loss Tensor"):
+            print(f"Loss Tensor: {loss}")
+
+        # Check whether the dots are leaves dots
+        with Beautifier("Leaf Tensors"):
+            print(f"Is X a leaf tensor? {green("True") if X.is_leaf else red("False")}")
+            print(f"Is y a leaf tensor? {green("True") if y.is_leaf else red("False")}")
+            print(f"Is W a leaf tensor? {green("True") if W.is_leaf else red("False")}")
+            print(f"Is b a leaf tensor? {green("True") if b.is_leaf else red("False")}")
+            print(f"Is y_hat a leaf tensor? {green("True") if Z.is_leaf else red("False")}")
+            print(f"Is loss a leaf tensor? {green("True") if loss.is_leaf else red("False")}")
+
+        # Perform backpropagation to compute gradients
+        loss.backward()
+        with Beautifier("Gradients"):
+            print(f"Gradient of W: {W.grad}")
+            print(f"Gradient of b: {b.grad}")
+            print(f"Gradient of X: {X.grad}")
+            print(f"Gradient of y: {y.grad}")
+
+
+if __name__ == "__main__":
+    main()

f42bf53cdac2bd43c425f239464ff764b66d0213
DaoChaShao <27882272@qq.com>
2025-10-18 00:30:31 +0800

    feat: add 19_DP_01_params_init_05_xavier.py for parameter initialization using Xavier initialization in a PyTorch model

---
+++ 19_DP_01_params_init_05_xavier.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/10/17 16:13
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   19_DP_01_params_init_05_xavier.py
+# @Desc     :   
+
+from torch import nn
+
+from utils.helper import Beautifier
+from utils.highlighter import lines
+
+
+def main() -> None:
+    """ Main Function """
+    with Beautifier("Parameters Initialization with Xavier Initialization"):
+        model = nn.Sequential(
+            nn.Linear(10, 2),
+        )
+
+        print("Model before initialization:")
+        for name, param in model.named_parameters():
+            print(
+                f"{name}:\nparams={param.data},\nmean={param.data.mean():.4f}, std={param.data.std():.4f}"
+            )
+
+        # Initialize parameters
+        lines("Initialize parameters")
+        for layer in model:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_normal_(layer.weight)
+                # Xavier initialisation is typically not applied to biases because they are 1d tensors normally.
+                # nn.init.xavier_normal_(layer.bias)
+
+        print("Model after initialization:")
+        for name, param in model.named_parameters():
+            print(
+                f"{name}:\nparams={param.data},\nmean={param.data.mean():.4f}, std={param.data.std():.4f}"
+            )
+
+
+if __name__ == "__main__":
+    main()
